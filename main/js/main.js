7d800232f98073228a20bfaa75dcc6b22f956e62
Casey C. Ross <caseycross718@gmail.com>
2017-04-28 16:56:56 -0400

    Everything works!
    Now I just have to figure out how to add the bells and whistles and
    make it look nice

--- main/js/main.js
+++ main/js/main.js
@@ -15,8 +15,7 @@ var Stamen_TonerLite = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_nolab
   ext: 'png'
 }).addTo(map);
 
-var extonLayer;
-
+var globalData;
 
 // Radio Button Testing Functions
 function rb1Selected(){
@@ -33,112 +32,150 @@ function rb2Selected(){
   console.log("Radio Button 2 has been selected," + " " + "rb2 button status =" + " " + rb2.checked);
 }
 
-// Carto Data Functions
-function Exton_Center() {
-  var Exton_Station = cartodb.createLayer(map, {
+// The dataPull function essentially creates a blank cartoDB later, which I can then change using station-s[ecific functions.
+// This solves several problems: it means any station can be mapped first, whereas before Exton 2011 had to be mapped first or
+// else stationData didn't exist; it makes it so that the SQL is truly refreshing and layers cannot appear on top of each other;
+// it is easier for me, personally, to work with and understand.
+function dataPull() {
+  var dataDummy = cartodb.createLayer(map, {
     user_name: 'caseyxbones',
     type: 'cartodb',
     sublayers:
-      [{
-      sql: "SELECT * FROM regionalrailstations_1 WHERE station LIKE '%Exton%'",
-      cartocss: "#layer { marker-width: 10; marker-fill: #000000; marker-fill-opacity: 0.9; marker-allow-overlap: true; marker-line-width: 1; marker-line-color: #FFF; marker-line-opacity: 1; }"
-      }]
-  })
-  .addTo(map)
-  ;
-  map.panTo(new L.LatLng(40.01943118, -75.62175724));
-  Exton_Center.fired = true;
+        [
+        {
+        sql: "",
+        cartocss: ""
+        },
+        {
+        sql: "",
+        cartocss: ""
+        }]
+      }, {}, function(layer) {
+        stationData = layer;
+      }).addTo(map);
+      dataPull.called = true;
+      console.log("dataPull status" + " " + "=" + dataPull.called);
+      return dataDummy;
+
 }
 
-function extonData() {
-  console.log("Exton Data called");
-  var extonDummy = cartodb.createLayer(map, {
-    user_name: 'caseyxbones',
-    type: 'cartodb',
-    sublayers:
-        [{
-        sql: "SELECT * FROM exton_2011_blocks",
-        cartocss: "#layer { polygon-fill: ramp([count_], (#ffffb2, #fecc5c, #fd8d3c, #f03b20, #bd0026), quantiles); line-width: 1; line-color: #FFF; line-opacity: 0.5; }"
-        }]
-  })
-  .addTo(map);
+dataPull(); // calls the dataPull function independently so that data is pulled down once page is loaded
+
+
+function exton2011() {
+    console.log("exton2011 called");
+    layerSelected = stationData.getSubLayer(0);
+    layerSelected.setSQL("SELECT * FROM exton_2011_blocks");
+    layerSelected.setCartoCSS("#layer { polygon-fill: ramp([count_], (#ffffb2, #fecc5c, #fd8d3c, #f03b20, #bd0026), quantiles); line-width: 1; line-color: #FFF; line-opacity: 0.5; }");
+    pointSelected = stationData.getSubLayer(1);
+    pointSelected.setSQL("SELECT * FROM regionalrailstations_1 WHERE station LIKE '%Exton%'");
+    pointSelected.setCartoCSS("#layer { marker-width: 10; marker-fill: #000000; marker-fill-opacity: 0.9; marker-allow-overlap: true; marker-line-width: 1; marker-line-color: #FFF; marker-line-opacity: 1; }");
 }
 
-function Exton2011() {
-  console.log("Exton2011 called");
-  var extonDummy11 = cartodb.createLayer(map, {
-    user_name: 'caseyxbones',
-    type: 'cartodb',
-    sublayers:
-        [{
-        sql: "SELECT * FROM exton_2011_blocks",
-        cartocss: "#layer { polygon-fill: ramp([count_], (#ffffb2, #fecc5c, #fd8d3c, #f03b20, #bd0026), quantiles); line-width: 1; line-color: #FFF; line-opacity: 0.5; }"
-        }]
-  })
-  .addTo(map);
-  return extonDummy11;
+function exton2016() {
+    console.log("exton2016 called");
+    layerSelected = stationData.getSubLayer(0);
+    layerSelected.setSQL("SELECT * FROM exton_2016_blocks");
+    layerSelected.setCartoCSS("#layer { polygon-fill: ramp([count_], (#c4e6c3, #80c799, #4da284, #2d7974, #1d4f60), quantiles); line-width: 1; line-color: #FFF; line-opacity: 0.5; }");
+    pointSelected = stationData.getSubLayer(1);
+    pointSelected.setSQL("SELECT * FROM regionalrailstations_1 WHERE station LIKE '%Exton%'");
+    pointSelected.setCartoCSS("#layer { marker-width: 10; marker-fill: #000000; marker-fill-opacity: 0.9; marker-allow-overlap: true; marker-line-width: 1; marker-line-color: #FFF; marker-line-opacity: 1; }");
 }
 
-function Exton2016() {
-  console.log("Exton2016 called");
-  var Exton_Dummy2 = cartodb.createLayer(map, {
-    user_name: 'caseyxbones',
-    type: 'cartodb',
-    sublayers:
-        [{
-        sql: "SELECT * FROM exton_2016_blocks",
-        cartocss: "#layer { polygon-fill: ramp([count_], (#c4e6c3, #80c799, #4da284, #2d7974, #1d4f60), quantiles); line-width: 1; line-color: #FFF; line-opacity: 0.5; }"
-        }]
-  })
-  .addTo(map);
+function thorndale2016() {
+    console.log("thorndale2016 called");
+    layerSelected = stationData.getSubLayer(0);
+    layerSelected.setSQL("SELECT * FROM thorndale_2016_blocks");
+    layerSelected.setCartoCSS("#layer { polygon-fill: ramp([count_], (#f3e79b, #fab27f, #eb7f86, #b95e9a, #5c53a5), quantiles); line-width: 1; line-color: #FFF; line-opacity: 0.5; }");
+    pointSelected = stationData.getSubLayer(1);
+    pointSelected.setSQL("SELECT * FROM regionalrailstations_1 WHERE station LIKE '%Thorndale%'");
+    pointSelected.setCartoCSS("#layer { marker-width: 10; marker-fill: #000000; marker-fill-opacity: 0.9; marker-allow-overlap: true; marker-line-width: 1; marker-line-color: #FFF; marker-line-opacity: 1; }");
 }
 
 // Station selection Functions
 function MapSelected() {
-  if ((rb1.checked === true)){
+  if (($("#station_name").text() === "Exton Station") && (rb1.checked === true)) {
     console.log("Someone wants to map Exton 2011!");
-    extonLayer = Exton2011();
+    exton2011();
+    return;
   }
-  else if ((rb2.checked === true)) {
-    console.log("Someone wants to map Exton 2016!");
-    Exton2016();
-  }
-  else {
-    alert("Please select data to map");
-    console.log("No data is selected!");
-  }
+    else if (($("#station_name").text() === "Exton Station") && (rb2.checked === true)) {
+      console.log("Someone wants to map Exton 2016!");
+      exton2016();
+      return;
+    }
+    else if (($("#station_name").text() === "Thorndale Station") && (rb1.checked === true)) {
+      console.log("Someone wants to map Thorndale 2016!");
+      thorndale2016();
+      return;
+    }
+    else {
+      alert("Please select data to map");
+      console.log("No data is selected!");
+    }
 }
 
 function showDropdown() {
+    showDropdown.called = true;
+    console.log("showDropdown.called()"+ " " + "=" + " " + showDropdown.called);
+    console.log("dataPull called within showDropdown");
     $("#myDropdown").show();
 }
 
-
 // Results Functions
-function Exton_Results(){
+function extonResults(){
   $("#myDropdown").hide();
   $("#station_name").text("Exton Station");
   $("#line_name").text("Paoli/Thorndale Line");
   $("#station_location").text("Chester, Pennsylvania");
   $("#station_location").text("Chester, Pennsylvania");
   $("#year1").text("2011");
+  $("#year2").show();
+  $("#rb2").show();
   $("#year2").text("2016");
   $("#results").show();
 }
 
+function thorndaleResults(){
+  $("#myDropdown").hide();
+  $("#station_name").text("Thorndale Station");
+  $("#line_name").text("Paoli/Thorndale Line");
+  $("#station_location").text("Chester, Pennsylvania");
+  $("#station_location").text("Chester, Pennsylvania");
+  $("#year1").text("2016");
+  $("#year2").hide();
+  $("#rb2").hide();
+  $("#results").show();
+}
 
 // Click Events
 $("#Home").click(function(){
+  console.log("Re-center map has been clicked");
+  stationData.hide();
   map.panTo(new L.LatLng(39.952372, -75.163584),8);
   $("#myDropdown").hide();
   $("#results").hide();
 });
 
 $("#Exton").click(function(){
-  Exton_Center();
-  Exton_Results();
+  console.log("Exton has been clicked in the dropdown menu");
+  console.log("Exton clearMap() has been called");
+  map.panTo(new L.LatLng(40.01943118, -75.62175724));
+  extonResults();
+});
+
+$("#Thorndale").click(function(){
+  console.log("Thorndale has been clicked in the dropdown menu");
+  console.log("Thorndale clearMap() has been called");
+  console.log("stationData.hide() executed");
+  map.panTo(new L.LatLng(39.99277222, -75.76289642));
+  thorndaleResults();
 });
 
 $("#mapSelected").click(function(){
+  console.log("The 'Map Selected' button has been clicked");
+  stationData.hide();
   MapSelected();
+  stationData.show();
+  console.log("stationData.show() executed");
 });
